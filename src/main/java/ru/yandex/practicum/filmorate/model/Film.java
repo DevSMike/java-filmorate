102d80e7c3f10718284cb7dec2790c62631e3f46
DevSMike <likashev.migha@yandex.ru>
2023-02-18 12:27:08 +0300

    feat: добавить дополнительные аннотации для валидации

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,21 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class Film {
-
 
     int id;
     @NonNull
     String name;
-    @NonNull
+    @NonNull @Length(min = 1, max = 200, message = "Описание фильма не должно превышать 200 символов.")
     String description;
-    @NonNull
+    @NonNull @PastOrPresent(message = "Дата не может быть в будущем")
     LocalDate releaseDate;
-    @NonNull
+    @NonNull @Min(value = 0, message = "Продолжительность фильма должна быть положительной")
     Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,20 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class User {
 
-     int id;
-    @NonNull
-    String email;
-    @NonNull
-    String login;
-
+    int id;
     String name;
-    @NonNull
+    @NonNull @Email(message = "Введено некорректное значение электронной почты.")
+    String email;
+    @NonNull @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp ="\\S*", message = "Логин не может содержать пробелы.")
+    String login;
+    @NonNull  @PastOrPresent(message = "Дата не может быть в будущем")
     LocalDate birthday;
 }
