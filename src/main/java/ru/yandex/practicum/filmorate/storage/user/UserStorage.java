f58812c4702aa0b48e7432b927e688ed79831387
DevSMike <likashev.migha@yandex.ru>
2023-03-03 18:50:15 +0300

    feat: добавить методы по выводу объектов списком и таблицей

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserStorage {
 
     void add (User user);
@@ -9,5 +12,9 @@ public interface UserStorage {
     void update (User user);
 
     void delete (User user);
+
+    List<User> getUsersList();
+
+    Map<Long, User> getUsersMap();
 
 }
