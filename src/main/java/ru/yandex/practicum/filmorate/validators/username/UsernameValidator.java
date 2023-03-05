5b42b0e52f7ac111556b948b34487ca24885ef89
DevSMike <likashev.migha@yandex.ru>
2023-03-05 17:26:32 +0300

    feat: добавить валидацию юзернейма

---
+++ src/main/java/ru/yandex/practicum/filmorate/validators/username/UsernameValidator.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.validators.username;
+
+import ru.yandex.practicum.filmorate.model.User;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UsernameValidator implements ConstraintValidator<CheckUsername, User> {
+    @Override
+    public boolean isValid(User user, ConstraintValidatorContext constraintValidatorContext) {
+        if (user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+        return true;
+    }
+}
