ac5d60e3a59ed4a6f7232972d03d402eb38d862c
DevSMike <likashev.migha@yandex.ru>
2023-04-02 13:09:15 +0300

    feat: добавить @NonBlank аннотацию к имени

--- src/main/java/ru/yandex/practicum/filmorate/model/film/Genres.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/film/Genres.java
@@ -3,10 +3,14 @@ package ru.yandex.practicum.filmorate.model.film;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 public class Genres {
+
     private int id;
+    @NotBlank
     private String name;
 
     public Genres(int id, String name) {
--- src/main/java/ru/yandex/practicum/filmorate/model/film/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/film/Mpa.java
@@ -3,11 +3,14 @@ package ru.yandex.practicum.filmorate.model.film;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 public class Mpa {
 
     private int id;
+    @NotBlank
     private String name;
 
     public Mpa(int id, String name) {
