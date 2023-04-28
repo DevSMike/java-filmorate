e6d85bb1c6137b60d45b9dfd42c78e75d4dc370c
DevSMike <likashev.migha@yandex.ru>
2023-04-29 01:22:32 +0300

    feat: добавить проверку genre ID

--- src/main/java/ru/yandex/practicum/filmorate/validators/id/EntityIdValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/id/EntityIdValidator.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.film.Genres;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.genres.GenresStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -14,6 +18,7 @@ public class EntityIdValidator implements IdCheck {
 
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
+    private final GenresStorage genresStorage;
 
     @Override
     public void checkFilmId(long id) {
@@ -37,6 +42,18 @@ public class EntityIdValidator implements IdCheck {
         if (id <= 0) {
             log.debug(id + " User is incorrect");
             throw new ValidationException("User id is incorrect");
+        }
+        log.debug(id + " passed validationId");
+    }
+
+    @Override
+    public void checkGenreId(int id) {
+        if (!genresStorage.getAllGenres().stream().collect(Collectors.toMap(Genres::getId, x->x)).containsKey(id)) {
+           log.debug(id + "failed to find genre id");
+           throw new NullPointerException("Genre id is not found");
+        }
+        if (id < 0) {
+            throw new ValidationException(id + " Genre id is incorrect");
         }
         log.debug(id + " passed validationId");
     }
